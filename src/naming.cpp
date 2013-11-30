d05d9fc62679e73831fb3d0749a4e15a27c35c13
Rémi Attab <remi.attab@gmail.com>
2013-11-30 11:48:58 -0500

    Added a simple in-process naming service.

---
+++ src/naming.cpp
@@ -0,0 +1,97 @@
+/* naming.cpp                                 -*- C++ -*-
+   Rémi Attab (remi.attab@gmail.com), 30 Nov 2013
+   FreeBSD-style copyright and disclaimer apply
+
+   Naming service implementation
+*/
+
+#include "naming.h"
+
+namespace slick {
+
+
+/******************************************************************************/
+/* LOCAL NAMING                                                               */
+/******************************************************************************/
+
+void
+LocalNaming::
+publish(const std::string& endpoint, Payload&& data)
+{
+    std::lock_guard<std::mutex> guard(lock);
+    endpoints[endpoint].pendingEvents.emplace_back(New, std::move(data));
+}
+
+void
+LocalNaming::
+retract(const std::string& endpoint, Payload&& data)
+{
+    std::lock_guard<std::mutex> guard(lock);
+    endpoints[endpoint].pendingEvents.emplace_back(Lost, std::move(data));
+}
+
+void
+LocalNaming::
+discover(const std::string& endpoint, const WatchFn& watch)
+{
+    std::lock_guard<std::mutex> guard(lock);
+    endpoints[endpoint].pendingWatches.emplace_back(watch);
+}
+
+void
+LocalNaming::
+shutdown()
+{
+    std::lock_guard<std::mutex> guard(lock);
+    shutdown = true;
+    endpoints.clear();
+}
+
+void
+LocalNaming::
+poll()
+{
+    std::lock_guard<std::mutex> guard(lock);
+    if (shutdown) return;
+
+    for (auto& endpoint : endpoints) {
+        auto& info = endpoint.second;
+
+        for (auto& watch : info.pendingWatches) processWatch(info, watch);
+        info.pendingWatches.clear();
+
+        for (auto& event : info.pendingEvents) processEvent(info, event);
+        info.pendingEvents.clear();
+    }
+}
+
+void
+LocalNaming::
+processWatch(EndpointInfo& info, Watch& watch)
+{
+    for (const auto& data : info.payloads)
+        watch(Payload(data));
+    watches.emplace_back(std::move(watch));
+}
+
+void
+LocalNaming::
+processEvent(EndpointInfo& info, EventInfo& event)
+{
+    for (const auto& watch : info.watches)
+        watch(event.type, Payload(event.data));
+
+    if (event.type == New)
+        payloads.emplace_back(std::move(event.data));
+
+    else {
+        auto it = std::find(
+                info.payloads.begin(), info.payloads.end(), event.data);
+        std::assert(it != info.payloads.end());
+
+        info.payloads.erase(it);
+    }
+}
+
+
+} // slick
--- src/naming.h
+++ src/naming.h
@@ -2,10 +2,12 @@
    Rémi Attab (remi.attab@gmail.com), 17 Nov 2013
    FreeBSD-style copyright and disclaimer apply
 
-   Naming service mock interaces.
+   Naming service interface.
 */
 
 #pragma once
+
+#include "payload.h"
 
 namespace slick {
 
@@ -16,7 +18,72 @@ namespace slick {
 
 struct Naming
 {
-    
+    enum Event { New, Lost };
+    typedef std::function<void(Event, Payload&&)> WatchFn;
+
+    virtual void poll() = 0;
+    virtual void shutdown() = 0;
+
+    virtual void publish(const std::string& endpoint, Payload&& data) = 0;
+    virtual void retract(const std::string& endpoint, Payload&& data) = 0;
+    virtual void discover(const std::string& endpoint, const WatchFn& watch) = 0;
+
+    void retract(const std::string& endpoint, const Payload& data)
+    {
+        retract(endpoint, Payload(data));
+    }
+
+    void publish(const std::string& endpoint, const Payload& data)
+    {
+        publish(endpoint, Payload(data));
+    }
+
+};
+
+
+/******************************************************************************/
+/* LOCAL NAMING                                                               */
+/******************************************************************************/
+
+struct LocalNaming : public Naming
+{
+    LocalNaming() : shutdown(false) {}
+
+    virtual void poll();
+    virtual void shutdown();
+
+    virtual void publish(const std::string& endpoint, Payload&& data);
+    virtual void retract(const std::string& endpoint, Payload&& data);
+    virtual void discover(const std::string& endpoint, const WatchFn& watch);
+
+private:
+
+    void processWatch(EndpointInfo& info, Watch& watch);
+    void processEvent(EndpointInfo& info, EventInfo& event);
+
+    struct EventInfo
+    {
+        EventInfo() {}
+        EventInfo(Event type, Payload&& data) :
+            type(type), data(std::move(data))
+        {}
+
+        Event type;
+        Payload data;
+    };
+
+    struct EndpointInfo
+    {
+        std::vector<WatchFn> watches;
+        std::vector<Payload> payloads;
+
+        std::vector<WatchFn> pendingWathes;
+        std::vector<EventInfo> pendingEvents;
+    };
+
+    std::mutex lock;
+    bool shutdown;
+    std::map<std::string, EndpointInfo> endpoints;
 };
 
 } // slick
